0e1114cbeeb9601da776e69a1033a00a606a0d2f
mateiaandreea04 <79466789+mateiaandreea04@users.noreply.github.com>
2021-02-28 19:24:15 +0000

    Add files via upload

---
+++ foxes-and-rabbits-v2-copy/Animal.java
@@ -0,0 +1,118 @@
+import java.util.List;
+import java.util.Random;
+/**
+ * A class representing shared characteristics of animals.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29 (2)
+ */
+public abstract class Animal
+{
+    // Whether the animal is alive or not.
+    private boolean alive;
+    // The animal's field.
+    private Field field;
+    // The animal's position in the field.
+    private Location location;
+    // The simulator the animal is in
+    protected Simulator simulator;
+    //variable that holds the gender value of an animal 0-male 1-female
+    private int gender;
+    //tracks if an animal is sleeping or not
+    private boolean isSleeping;
+    
+    private Random rand;
+    
+    
+    /**
+     * Create a new animal at location in field.
+     * 
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     */
+    public Animal(Field field, Location location)
+    {
+        alive = true;
+        this.field = field;
+        setLocation(location);
+        setGender();
+    }
+    
+    /**
+     * Make this animal act - that is: make it do
+     * whatever it wants/needs to do.
+     * @param newAnimals A list to receive newly born animals.
+     */
+    abstract public void act(List<Animal> newAnimals);
+
+    /**
+     * Check whether the animal is alive or not.
+     * @return true if the animal is still alive.
+     */
+    protected boolean isAlive()
+    {
+        return alive;
+    }
+    
+    protected void setGender()
+    {
+        Random rand = Randomizer.getRandom();     
+        gender = rand.nextInt(2);
+    }
+    
+    protected int getGender(){
+        return gender;
+    }
+    /**
+     * Indicate that the animal is no longer alive.
+     * It is removed from the field.
+     */
+    protected void setDead()
+    {
+        alive = false;
+        if(location != null) {
+            field.clear(location);
+            location = null;
+            field = null;
+        }
+    }
+
+    /**
+     * Return the animal's location.
+     * @return The animal's location.
+     */
+    protected Location getLocation()
+    {
+        return location;
+    }
+    
+    /**
+     * Place the animal at the new location in the given field.
+     * @param newLocation The animal's new location.
+     */
+    protected void setLocation(Location newLocation)
+    {
+        if(location != null) {
+            field.clear(location);
+        }
+        location = newLocation;
+        field.place(this, newLocation);
+    }
+    
+    /**
+     * Return the animal's field.
+     * @return The animal's field.
+     */
+    protected Field getField()
+    {
+        return field;
+    }        
+    
+    public void sleep(boolean status){
+        isSleeping = status;
+    }
+    
+    public boolean getIsSleeping(){
+        return isSleeping;
+    }
+}
